4bd4f9d4464d4a175224d8777deda5ba31b19896
DianaBeharvandi <94246585+DianaBeharvandi@users.noreply.github.com>
2022-05-30 12:42:45 +0300

    v1.2
    updated the logic to cover better all possible cases

--- SlotMachine.js
+++ SlotMachine.js
@@ -1,21 +1,45 @@
 let total = 0;
-let canPlay = true;
-document.getElementById("total").innerHTML = total;
+let betMoney = 0;
+let canPlay = false;
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * 2) + 1;
-        
+    return Math.floor(Math.random() * 2) + 1;    
 }
 
-function getElement(id) {
-    return document.getElementById(id);
+function changeTotal() {
+    let addMoney = Number.parseInt(document.getElementById("add_balance").value);
+    if(addMoney) {
+        total += addMoney;
+        document.getElementById("total").innerHTML = total;
+        canPlay = true;
+    } else {
+        document.getElementById("total").innerHTML = ` ${total}
+        <div> You need to add money!!!  </div>`; 
+    }
+}
+
+function bet() {
+    betMoney = Number.parseInt(document.getElementById ("bet_amount").value);
+    if (betMoney && betMoney <= total) {
+        document.getElementById("total").innerHTML = total;
+        canPlay = true;
+    } else {
+        document.getElementById("total").innerHTML = ` ${total}
+            <div> You need to add money or lower your bet!!!  </div>`;
+        canPlay = false;
+    }
 }
 
 function spin() {
-    if (total > 0 && canPlay ) {
-        const item1 = getElement('item1');
-        const item2 = getElement('item2');
-        const item3 = getElement('item3');
+    if (!total) { document.getElementById("total").innerHTML = ` ${total}
+        <div> You need to add money!!!  </div>`;    
+    } else if (!betMoney) {
+        document.getElementById("total").innerHTML = ` ${total}
+            <div> You need to place a bet!!  </div>`;
+    } else if (total > 0 && canPlay && betMoney <= total) {
+        const item1 = document.getElementById('item1');
+        const item2 = document.getElementById('item2');
+        const item3 = document.getElementById('item3');
 
         const num1 = getRandomNumber();
         const num2 = getRandomNumber();
@@ -25,31 +49,25 @@ function spin() {
         item2.innerHTML = `${num2}`;
         item3.innerHTML = `${num3}`;
 
-        let betAmount = bet();
-        if (betAmount <= total) { 
-            if (num1 === num2 && num1 === num3) {
-                showMessage();
-                total += 2 * betAmount;
-                document.getElementById("total").innerHTML = total;
-            } else {
-                hideMessage();
-                total -= betAmount;
-                document.getElementById("total").innerHTML = total;
-            } 
+        if (num1 === num2 && num1 === num3) {
+            showMessage();
+            total += 2 * betMoney;
+            document.getElementById("total").innerHTML = total;
         } else {
-            document.getElementById("total").innerHTML = ` ${total}
-            <div> You need to add money!!!  </div>`;
-        }
+            hideMessage();
+            total -= betMoney;
+            document.getElementById("total").innerHTML = total;
+        } 
     } else {
         document.getElementById("total").innerHTML = ` ${total}
-            <div> You need to add money!!!  </div>`;
+            <div> You need to change Bet!!!  </div>`;
     }
 }
     
 function showMessage() {
     const msg = document.getElementById("message");
     msg.style.display = "block";
-    msg.classList.add('animated', 'pulse')
+
 }
 
 function hideMessage() {
@@ -57,21 +75,3 @@ function hideMessage() {
     msg.style.display = "none";
 }
    
-function changeTotal() {
-    let addMoney = document.getElementById("add_balance").value;
-    total += Number.parseInt(addMoney);
-    document.getElementById("total").innerHTML = total;
-    canPlay = true;
-}
-
-function bet() {
-    let betMoney = document.getElementById ("bet_amount").value;
-    if (Number.parseInt(betMoney) && Number.parseInt(betMoney) <= total) {
-        return Number.parseInt(betMoney);
-        canPlay = true;
-    } else {
-        document.getElementById("total").innerHTML = ` ${total}
-            <div> You need to add money or lower your bet!!!  </div>`;
-        canPlay = false;
-    }
-}
